9df40f50ef5ab7e15b5ea8bf7cc5910d5173950a
Jonathan Warner <jewarner57@gmail.com>
2021-04-13 18:32:52 -0400

    feat: complete page one

--- src/routes/main.js
+++ src/routes/main.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (req, res) => {
-  return res.send("Its working");
+  return res.render('home')
 });
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -6,19 +6,15 @@ import routes from './routes';
 
 const app = express();
 app.use(cors())
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-// Custom app level middleware
-app.use((req, res, next) => {
-  req.context = {
-    models,
-    me: models.users[1],
-  };
-  next();
-});
+
+// Add handlebars
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 app.use('/', routes.main);
 
 app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port ${process.env.PORT}`),
+  console.log(`App listening on port ${process.env.PORT}`),
 );
