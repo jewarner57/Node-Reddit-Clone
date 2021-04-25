bd46023c06152f3662ef7c0d1fd6cd0e6840d146
Jonathan Warner <jewarner57@gmail.com>
2021-04-25 17:35:50 -0400

    add author to comments

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -39,11 +39,12 @@ module.exports = (app) => {
       })
   })
 
+  // SHOW
   app.get("/posts/:id", function (req, res) {
     var currentUser = req.user;
     // LOOK UP THE POST
 
-    Post.findById(req.params.id).lean().populate('comments').populate('author')
+    Post.findById(req.params.id).lean().populate({ path: 'comments', populate: { path: 'author' } }).populate('author')
       .then(post => {
         res.render("posts-show", { post, currentUser });
       })
